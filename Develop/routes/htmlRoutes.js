cb99bd13292ca45a6762f0a36cab1d7c3b11d84c
John Leamy <jwleamy@gmail.com>
2023-01-30 17:30:27 -0800

    htmlroutes

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,9 +1,19 @@
 const path = require("path");
 
 module.exports = function(app) {
-    // GET request
+    // GET request HTML
     app.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, "../Develop/public/notes.html"));
     }); 
+
+    // Get request CSS
+    app.get("/styles", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Develop/public/assets/css/styles.css"));
+    });
+
+    // Get request to return homepage to the index.html
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+    });
 
 }
