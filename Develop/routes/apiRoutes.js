4fbdd21da7280bb48d2b604982289e917a90f5ba
John Leamy <jwleamy@gmail.com>
2023-01-30 17:23:35 -0800

    post request

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,11 +2,16 @@ const allNotes = require("../db/db.json");
 
 
 module.exports = function(app) { 
-    // GET request
+    
+    // GET request - retrieves and presents data (notes) to the user
     app.get("/api/notes", (req, res) => {
         res.json(allNotes);
     }); 
 
-
+    // Post requests -  allows users to post data (notes) to the database
+    app.post("/api/notes", (req, res) => {
+        notesData.push(req.body);
+        res.json("Saved");
+    });
 
 }
