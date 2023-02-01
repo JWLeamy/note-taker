2cdd8023ede608ccfb4d5811eb724e7d81988278
John Leamy <jwleamy@gmail.com>
2023-02-01 13:10:57 -0800

    troubleshooting

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -4,13 +4,15 @@ const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
-const express = require('express');
-const app = express();
+const app = require("express").Router();
+
 
 // GET request - retrieves and presents data (notes) to the user
 app.get("/notes", (req, res) => {
   readFileAsync("db/db.json", "utf8").then(function (data) {
     allNotes = JSON.parse(data);
+    console.log("notes have rendered");
+    console.log(allNotes);
     res.json(allNotes);
   });
 });
@@ -37,6 +39,23 @@ app.post("/notes", (req, res) => {
     });
     res.json(allNotes);
   });
+
+});
+
+app.delete("/notes/:id", (req, res) => {
+  let selID = parseInt(req.params.id);
+  //  Read JSON file
+  for (let i = 0; i < allNotes.length; i++) {
+    if (selID === allNotes[i].id) {
+      allNotes.splice(i, 1);
+      let noteJSON = JSON.stringify(allNotes, null, 2);
+
+      writeFileAsync("db/db.json", noteJSON).then(function () {
+        console.log("Note has been deleted.");
+      });
+    }
+  }
+  res.json(allNotes);
 });
 
 module.exports = app;
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,11 +4,14 @@ const router = require("express").Router()
     // GET request HTML
     router.get("/notes", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
+        console.log("htmlroutes working notes");
     }); 
 
     // Get request to return homepage to the index.html
     router.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+        console.log("htmlroutes working index");
+
     });
 
 module.exports = router;
