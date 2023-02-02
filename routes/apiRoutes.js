d867a2831b80ab45dd1cdc86a7e7b01f1fee838b
John Leamy <jwleamy@gmail.com>
2023-02-02 13:13:57 -0800

    rewrote code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,28 +1,22 @@
 // Set variable now to avoid "let"/"var" later
-const fs = require('fs');
 const allNotes = require('../db/db.json');
 const uniqid = require('uniqid');
+const fs = require('fs');
 
-module.exports = function(app) {
+module.exports = (app) => {
+  app.get('/api/notes', (req, res) => res.json(allNotes));
 
-    app.get('/api/notes', (req, res) => {
-      res.json(allNotes);
-      console.log('yes');
-    });
-    
-  
-    app.post('/api/notes', (req, res) => {
-      req.body['id'] = uniqid();
-      allNotes.push(req.body);
-      res.json(true);
-      fs.writeFileSync('./db/db.json', JSON.stringify(allNotes))
-    })
-  
-    app.delete('/api/notes/:id', (req, res) => {
-      let findId = allNotes.findIndex(x => x.id === req.params.id);
-      allNotes.splice(findId, 1); 
-      fs.writeFileSync('./db/db.json', JSON.stringify(allNotes));
-      res.end();
-    })
-  
+  app.post('/api/notes', (req, res) => {
+    req.body['id'] = uniqid();
+    allNotes.push(req.body);
+    res.json(true);
+    fs.writeFileSync('./db/db.json', JSON.stringify(allNotes));
+  });
+
+  app.delete('/api/notes/:id', (req, res) => {
+    let findId = allNotes.findIndex((x) => x.id === req.params.id);
+    allNotes.splice(findId, 1);
+    fs.writeFileSync('./db/db.json', JSON.stringify(allNotes));
+    res.end();
+  });
 }
